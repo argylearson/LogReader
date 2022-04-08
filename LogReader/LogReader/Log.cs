27b8855bb3baf59950ea8a902a63808ab44586dc
Richard Lemke <rlemke@iu.edu>
2022-04-07 20:46:30 -0400

    Printing Output Correctly
    Next step is to output to a file.

--- LogReader/LogReader/Log.cs
+++ LogReader/LogReader/Log.cs
@@ -9,22 +9,37 @@ namespace LogReader
         public string level;
         public Guid id;
         public string message;
+        public bool isStart;
 
         private const string timeFormat = "yyyy-MM-dd HH:mm:ss,fff";
         private static readonly IFormatProvider culture = new CultureInfo("en-US");
+        public string timeStampString {
+            get {
+                return timeStamp.ToString(timeFormat);
+            }
+        }
 
         //current log format
         //2021-08-02 12:18:54,549 templogger   WARNING  "d9729770-87a6-452d-a8d2-c3f7dc354db8" "Articles api starts"
         public Log(string log)
         {
-            var datum = log.Split(' ');
+            var datum = log.Split(' ').Where(x => !string.IsNullOrWhiteSpace(x)).ToArray();
             var index = 0;
             timeStamp = DateTime.ParseExact($"{datum[index++]} {datum[index++]}", timeFormat, culture);
+            source = datum[index++];
+            level = datum[index++];
+            id = Guid.Parse(datum[index++].Trim('"'));
+
+            message = datum[index++].TrimStart('"');
+            while (index < datum.Length)
+                message += $" {datum[index++]}";
+            message = message.TrimEnd('"');
+            isStart = datum[index - 1].EndsWith("starts\"");
         }
 
         public override string ToString()
         {
-            return timeStamp.ToString("yyyy-MM-dd HH:mm:ss,fff");
+            return $"{message}, {timeStamp}";
         }
     }
 }
--- LogReader/LogReader/Program.cs
+++ LogReader/LogReader/Program.cs
@@ -1,11 +1,36 @@
-﻿using LogReader;
-
-Console.WriteLine("Hello, World!");
-
-var filePath = @"c:\api.log";
-
-foreach (var row in File.ReadLines(filePath))
+﻿namespace LogReader
 {
-    var log = new Log(row);
-    Console.WriteLine(log);
+    public class Program
+    {
+        private static string filePath = @"c:\api.log";
+        private static Dictionary<string, Log> startActivities = new Dictionary<string, Log>();
+
+        public static void Main(string[] args)
+        {
+            try
+            {
+                foreach (var row in File.ReadLines(filePath))
+                {
+                    var log = new Log(row);
+
+                    var activityExists = startActivities.TryGetValue(log.message, out var existingActivity);
+
+                    //we expect start activities to always preceed end activities
+                    //so this will write to our changed file
+                    if (activityExists)
+                        Console.WriteLine(WriteActivity(existingActivity, log));
+                    else
+                        startActivities.Add(log.message, log);
+                }
+            } catch
+            {
+                throw;
+            }
+        }
+
+        private static string WriteActivity(Log startLog, Log endLog)
+        {
+            return $"{startLog.message} - {endLog.message}, {startLog.timeStampString}, {endLog.timeStampString}, {(endLog.timeStamp - startLog.timeStamp).TotalSeconds}";
+        }
+    }
 }
