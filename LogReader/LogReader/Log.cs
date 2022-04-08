5a53f031c0e6789f426d4b435d409a3b0f0563bf
Richard Lemke <rlemke@iu.edu>
2022-04-07 20:06:31 -0400

    Reading and Time Stamp Parsing

---
+++ LogReader/LogReader/Log.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace LogReader
+{
+    internal class Log
+    {
+        public DateTime timeStamp;
+        public string source;
+        public string level;
+        public Guid id;
+        public string message;
+
+        private const string timeFormat = "yyyy-MM-dd HH:mm:ss,fff";
+        private static readonly IFormatProvider culture = new CultureInfo("en-US");
+
+        //current log format
+        //2021-08-02 12:18:54,549 templogger   WARNING  "d9729770-87a6-452d-a8d2-c3f7dc354db8" "Articles api starts"
+        public Log(string log)
+        {
+            var datum = log.Split(' ');
+            var index = 0;
+            timeStamp = DateTime.ParseExact($"{datum[index++]} {datum[index++]}", timeFormat, culture);
+        }
+
+        public override string ToString()
+        {
+            return timeStamp.ToString("yyyy-MM-dd HH:mm:ss,fff");
+        }
+    }
+}
--- LogReader/LogReader/Program.cs
+++ LogReader/LogReader/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using LogReader;
+
 Console.WriteLine("Hello, World!");
+
+var filePath = @"c:\api.log";
+
+foreach (var row in File.ReadLines(filePath))
+{
+    var log = new Log(row);
+    Console.WriteLine(log);
+}
