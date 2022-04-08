af18bcd33717007e8113fc4fe48f6a666bf3ccad
Richard Lemke <rlemke@iu.edu>
2022-04-07 21:05:48 -0400

    Log Writing Works
    Now to make fixes based on my better understanding of the log files

--- LogReader/LogReader/Program.cs
+++ LogReader/LogReader/Program.cs
@@ -1,15 +1,21 @@
-﻿namespace LogReader
+﻿using System.Text;
+
+namespace LogReader
 {
     public class Program
     {
-        private static string filePath = @"c:\api.log";
+        private static string inputFilePath = @"c:\api.log";
+        private static string outputFilePath = $"{Directory.GetCurrentDirectory()}\\api.csv";
         private static Dictionary<string, Log> startActivities = new Dictionary<string, Log>();
 
-        public static void Main(string[] args)
+        public async static Task Main(string[] args)
         {
             try
             {
-                foreach (var row in File.ReadLines(filePath))
+                using var file = File.Create(outputFilePath);
+                var linesToWrite = new List<string>();
+
+                foreach (var row in File.ReadLines(inputFilePath))
                 {
                     var log = new Log(row);
 
@@ -18,11 +24,17 @@
                     //we expect start activities to always preceed end activities
                     //so this will write to our changed file
                     if (activityExists)
-                        Console.WriteLine(WriteActivity(existingActivity, log));
+                        await file.WriteAsync(new UTF8Encoding(true).GetBytes(WriteActivity(existingActivity, log)));
+                    //linesToWrite.Add(WriteActivity(existingActivity, log));
                     else
                         startActivities.Add(log.message, log);
                 }
-            } catch
+
+                //using var file = File.Create(outputFilePath);
+                //await file.w//.WriteAllLinesAsync(linesToWrite);
+                file.Close();
+            }
+            catch
             {
                 throw;
             }
@@ -30,7 +42,7 @@
 
         private static string WriteActivity(Log startLog, Log endLog)
         {
-            return $"{startLog.message} - {endLog.message}, {startLog.timeStampString}, {endLog.timeStampString}, {(endLog.timeStamp - startLog.timeStamp).TotalSeconds}";
+            return $"{startLog.message} - {endLog.message}, {startLog.timeStampString}, {endLog.timeStampString}, {(endLog.timeStamp - startLog.timeStamp).TotalSeconds}\n";
         }
     }
 }
